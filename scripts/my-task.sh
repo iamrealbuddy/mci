a682f4110256296272de81c5b009d8440369f61b
Real Buddy <realbuddy@gmail.com>
2024-05-21 22:05:39 +0530

    Used for loop

--- scripts/my-task.sh
+++ scripts/my-task.sh
@@ -4,11 +4,11 @@ x=1
 echo "pwd is..."
 pwd
 #echo start > .done
-#printf "task running"
-while [ $x -le 10 ]
+printf "task running"
+for (( i=1; i <= 10; ++i ))
 do
-#  printf .
+  printf .
   sleep 1
-  x=$(( $x + 1 ))
+#  x=$(( $x + 1 ))
 done
 echo done > .done
