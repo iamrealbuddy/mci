3aab03cff8f927d34699a8d38666b14c19f8b7b7
Real Buddy <realbuddy@gmail.com>
2024-05-21 21:23:34 +0530

    Removed print

--- scripts/my-task.sh
+++ scripts/my-task.sh
@@ -3,11 +3,11 @@
 x=1
 echo "pwd is..."
 pwd
-echo start > .done
-printf "task running"
+#echo start > .done
+#printf "task running"
 while [ $x -le 10 ]
 do
-  printf .
+#  printf .
   sleep 1
   x=$(( $x + 1 ))
 done
