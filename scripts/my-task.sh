5e0acb88c4103212ed4ed4cac14bd1ebda11f7a7
Real Buddy <realbuddy@gmail.com>
2024-05-22 13:48:17 +0530

    Replace while with for

--- scripts/my-task.sh
+++ scripts/my-task.sh
@@ -5,10 +5,11 @@ echo "pwd is..."
 pwd
 #echo start > .done
 printf "task running"
-for (( i=1; i <= 10; ++i ))
+#for (( i=1; i <= 10; ++i ))
+while [ $x -le 10 ]
 do
   printf .
   sleep 1
-#  x=$(( $x + 1 ))
+  x=$(( $x + 1 ))
 done
 echo done > .done
