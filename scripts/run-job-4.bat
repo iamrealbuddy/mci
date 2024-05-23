093e78d0eafcee7c98b5c80957bfe5281931cea6
Real Buddy <realbuddy@gmail.com>
2024-05-23 13:49:55 +0530

    Added run

---
+++ scripts/run-job-4.bat
@@ -0,0 +1,67 @@
+@echo off
+
+rem run pipeline job defn.
+
+wsl docker info
+echo.
+
+set /p stk=start docker stack....
+if "%stk%" neq "" (
+ start run-new.bat
+ pause
+)
+
+rem set job=my-job-py-alpine
+set job=my-task-4
+set scm=D:\dev\github\mci
+set jenkh=D:\dev\apps\jenkins
+set javah=D:\app\dev\jdk-18.0.2
+
+echo.
+echo job list...
+%javah%\bin\java -jar %jenkh%\data\war\WEB-INF\lib\cli-2.455.jar -auth my-admin:admin -s http://localhost:8080 list-jobs
+pause
+
+set /p dlj=delete job %job%...
+if "%dlj%" neq "" (
+  echo deleting...
+  %javah%\bin\java -jar data\war\WEB-INF\lib\cli-2.455.jar -auth my-admin:admin -s http://localhost:8080 delete-job %job%
+  echo.
+  echo job list...
+  %javah%\bin\java -jar %jenkh%\data\war\WEB-INF\lib\cli-2.455.jar -auth my-admin:admin -s http://localhost:8080 list-jobs
+  pause
+)
+
+echo.
+echo search job %job%
+SETLOCAL ENABLEDELAYEDEXPANSION
+set jbf=
+FOR /F "tokens=* USEBACKQ" %%F IN (`%javah%\bin\java -jar %jenkh%\data\war\WEB-INF\lib\cli-2.455.jar -auth my-admin:admin -s http://localhost:8080 list-jobs`) DO (
+  if "%%F" equ "%job%" (
+    set jbf=%%F
+    echo job %job% found...
+  )
+)
+
+rem echo.
+rem echo jbf is....%jbf%
+rem pause
+
+if "%jbf%" equ "" (
+  echo job %job% not found....create
+  %javah%\bin\java -jar %jenkh%\data\war\WEB-INF\lib\cli-2.455.jar -auth my-admin:admin -s http://localhost:8080 create-job %job% < %scm%\jobs\%job%.xml
+  echo.
+  echo job list...
+  %javah%\bin\java -jar %jenkh%\data\war\WEB-INF\lib\cli-2.455.jar -auth my-admin:admin -s http://localhost:8080 list-jobs
+  pause
+)
+ENDLOCAL
+
+echo.
+echo start job %job%...
+pause
+%javah%\bin\java -jar data\war\WEB-INF\lib\cli-2.455.jar -auth my-admin:admin -s http://localhost:8080 build -s -v %job%
+pause
+
+%scm%\scripts\track-job.bat
+pause
--- scripts/track-job.bat
+++ scripts/track-job.bat
@@ -15,7 +15,7 @@ rem echo curl-1 el=%errorlevel%
 
 :loop
 timeout /t 1 >nul
-curl -s -u my-admin:admin http://localhost:8080/job/my-task-3/3/execution/node/2/ws/mci/scripts/.done | findstr -m "done" > nul
+curl -s -u my-admin:admin http://localhost:8080/job/my-task-4/1/execution/node/3/ws/mci/scripts/.done | findstr -m "done" > nul
 if %errorlevel% equ 0 echo DONE found...
 if %errorlevel% equ 1 (
   echo DONE not found...
